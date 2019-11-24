db7fb00e4c3a5c4f883938af80ae7de7131e3dfe
scragly <29337040+scragly@users.noreply.github.com>
2019-11-24 12:11:16 +1000

    Update embed helper, add new formatting helpers.

--- firetail/utils/formatters.py
+++ firetail/utils/formatters.py
@@ -21,14 +21,18 @@ def colour(*args):
         return discord.Colour.lighter_grey()
 
 
-def make_embed(msg_type='', title=None, title_url=None, icon=None,
-               content=None, msg_colour=None, guild=None):
-    """Returns a formatted discord embed object.
-
-    Define either a type or a colour, not both.
-    Types are:
-    error, warning, info, success.
+def make_embed(
+    msg_type='', title=None, icon=discord.Embed.Empty, content=None, msg_colour=None, guild=None,
+    title_url=discord.Embed.Empty, thumbnail='', image='', fields=None, footer=None, footer_icon=None, inline=False,
+    subtitle=None, subtitle_url=None
+):
     """
+    Helper for generating a formatted embed.
+
+    Types available:
+    error, warning, info, success, help.
+    """
+
     embed_types = {
         'error': {
             'icon': 'https://i.imgur.com/juhq2uJ.png',
@@ -51,18 +55,85 @@ def make_embed(msg_type='', title=None, title_url=None, icon=None,
             'colour': 'blue'
         }
     }
+
     if msg_type in embed_types.keys():
         msg_colour = embed_types[msg_type]['colour']
         icon = embed_types[msg_type]['icon']
+
     if guild and not msg_colour:
         msg_colour = colour(guild)
     else:
-        msg_colour = colour(msg_colour)
-    embed = discord.Embed(description=content, colour=msg_colour)
-    if not title_url:
-        title_url = discord.Embed.Empty
-    if not icon:
-        icon = discord.Embed.Empty
+        if not isinstance(msg_colour, discord.Colour):
+            msg_colour = colour(msg_colour)
+
+    embed = discord.Embed(description=content, colour=msg_colour, title=subtitle, url=subtitle_url)
+
     if title:
         embed.set_author(name=title, icon_url=icon, url=title_url)
+
+    if thumbnail:
+        embed.set_thumbnail(url=thumbnail)
+
+    if image:
+        embed.set_image(url=image)
+
+    fields = fields or {}
+    for key, value in fields.items():
+        ilf = inline
+        if not isinstance(value, str):
+            ilf = value[0]
+            value = value[1]
+        embed.add_field(name=key, value=value, inline=ilf)
+
+    if footer:
+        footer = {'text': footer}
+
+        if footer_icon:
+            footer['icon_url'] = footer_icon
+
+        embed.set_footer(**footer)
     return embed
+
+
+def bold(msg: str):
+    """Format to bold markdown text"""
+    return f'**{msg}**'
+
+
+def italics(msg: str):
+    """Format to italics markdown text"""
+    return f'*{msg}*'
+
+
+def bolditalics(msg: str):
+    """Format to bold italics markdown text"""
+    return f'***{msg}***'
+
+
+def code(msg: str):
+    """Format to markdown code block"""
+    return f'```{msg}```'
+
+
+def pycode(msg: str):
+    """Format to code block with python code highlighting"""
+    return f'```py\n{msg}```'
+
+
+def ilcode(msg: str):
+    """Format to inline markdown code"""
+    return f'`{msg}`'
+
+
+def convert_to_bool(argument):
+    lowered = argument.lower()
+    if lowered in ('yes', 'y', 'true', 't', '1', 'enable', 'on'):
+        return True
+    elif lowered in ('no', 'n', 'false', 'f', '0', 'disable', 'off'):
+        return False
+    else:
+        return None
+
+
+def bitround(x):
+    return max(min(1 << int(x).bit_length() - 1, 1024), 16)
